7f8ca4ce49660cf0ca67b59ca826056fb2015b94
profegil <profe.gilberto.ramirez@gmail.com>
2023-01-19 15:24:34 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+Ingeniería del conocimiento<br>
+escribe tu nombre <br>
+
+Minería de datos<br>
+
+<a href="https://polizona.com/mercado/XX/agrupacion.php/">Agrupación</a>
+
+<a href="https://polizona.com/mercado/XX/clasificacion.php/">Clasificación</a>
+
+</body>
+</html>
