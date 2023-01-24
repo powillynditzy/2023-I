1b9a2efd03cb188dcec0d93f75689a61dbfc3a7d
profegil <profe.gilberto.ramirez@gmail.com>
2023-01-24 05:18:08 -0600

    Create indexIN.php

---
+++ indexIN.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+Inteligencia de Negocios<br>
+escribe tu nombre <br>
+
+Minería de datos<br>
+
+<a href="agrupacion.php">Agrupación</a>
+
+<a href="clasificacion.php/">Clasificación</a>
+
+</body>
+</html>
