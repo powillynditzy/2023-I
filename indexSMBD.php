2438d3990fe1bd3ea4151a4ccf11337f192948ae
profegil <profe.gilberto.ramirez@gmail.com>
2023-01-24 05:15:18 -0600

    Create indexSMBD.php

---
+++ indexSMBD.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+Sistemas Manejadores de Bases de datos<br>
+escribe tu nombre <br>
+
+Minería de datos<br>
+
+<a href="agrupacion.php">Agrupación</a>
+
+<a href="clasificacion.php/">Clasificación</a>
+
+</body>
+</html>
