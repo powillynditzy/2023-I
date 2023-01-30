ea432a54277f58f708c0ad11fe801b6032de65e1
powillynditzy <123911192+powillynditzy@users.noreply.github.com>
2023-01-29 19:15:58 -0600

    Update global.php

--- global.php
+++ global.php
@@ -1,6 +1,6 @@
 <?php
     define("HOSTNAME", "68.70.164.26");
-    define("BD", "polizona_XX");
-    define("USER", "polizona_XX");
-    define("PASSWORD", "tu_contraseña");
+    define("BD", "polizona_mercado");
+    define("USER", "polizona_102");
+    define("PASSWORD", "Tres*Cuatro=12");
 ?>
